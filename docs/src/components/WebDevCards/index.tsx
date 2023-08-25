cf920464755142abbef9ed0e4a8dd92c358b715c
Sungwoo Cho <swcho@friendli.ai>
2023-08-25 16:44:18 +0900

    Periflow Container doc (#3)

--- docs/src/components/WebDevCards/index.tsx
+++ docs/src/components/WebDevCards/index.tsx
@@ -23,151 +23,54 @@ SOFTWARE.
 /* eslint-disable global-require */
 
 import React from 'react';
-import clsx from 'clsx';
-import Translate from '@docusaurus/Translate';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
 
-import get_started from '@site/static/img/docs/tutorial/get_started.png';
-import hf_bert from '@site/static/img/docs/tutorial/hf_bert.png';
-import hf_bart from '@site/static/img/docs/tutorial/hf_bart.png';
-import pretrain_wiki from '@site/static/img/docs/tutorial/pretrain_wiki.png';
-import pretrain_mt from '@site/static/img/docs/tutorial/pretrain_mt.png';
-import resnet_cifar from '@site/static/img/docs/tutorial/resnet_cifar.png';
+import cloud_img from '@site/static/img/docs/tutorials/cloud.png';
+import container_img from '@site/static/img/docs/tutorials/container.png';
 
 
 const WebDevCards = [
-  // {
-  //   name: 'Get started',
-  //   Svg: require('@site/static/img/docs/tutorial/get_started.svg').default,
-  //   url: {
-  //     page: 'get_started',
-  //   },
-  //   description: (
-  //     <div>
-  //       Get started with PeriFlow by a basic PyTorch example.
-  //     </div> 
-  //   ),
-  // },
   {
-    name: 'Get started',
-    png: get_started,
+    name: 'PeriFlow Cloud',
+    png: cloud_img,
     url: {
-      page: 'get_started',
+      page: 'cloud/how_to_use_your_checkpoint',
     },
-    description: (
-      <div>
-        Get started with PeriFlow by a basic PyTorch example.
-      </div> 
-    ),
+    description: 'Check out how to use PeriFlow as a managed service.'
   },
   {
-    name: 'Fine-tuning BERT with Hugging Face',
-    png: hf_bert,
+    name: 'PeriFlow Container',
+    png: container_img,
     url: {
-      page: 'hf_bert_ft',
+      page: 'container/how_to_run_periflow_container',
     },
-    description: (
-      <div>
-        Learn to fine-tune BERT with HuggingFace using PeriFlow.
-      </div> 
-    ),
-  },
-  {
-    name: 'Fine-tuning BART with Hugging Face',
-    png: hf_bart,
-    url: {
-      page: 'hf_bart_ft',
-    },
-    description: (
-      <div>
-        Learn to fine-tune BART with HuggingFace using Periflow.
-      </div> 
-    ),
-  },
-  {
-    name: 'Pre-train GPT-2 with WikiText-2',
-    png: pretrain_wiki,
-    url: {
-      page: 'pth_arlm',
-    },
-    description: (
-      <div>
-        Learn to pre-train GPT-2 with WikiText-2 using PeriFlow.
-      </div> 
-    ),
-  },
-  {
-    name: 'Pre-train GPT with Megatron-LM',
-    png: pretrain_mt,
-    url: {
-      page: 'megatron',
-    },
-    description: (
-      <div>
-        Learn to pre-train GPT with Megatron-LM using PeriFlow.
-      </div> 
-    ),
-  },
-  {
-    name: 'Training ResNet with CIFAR',
-    png: resnet_cifar,
-    url: {
-      page: 'cifar_10',
-    },
-    description: (
-      <div>
-        Learn to train ResNet on the CIFAR dataset using PeriFlow.
-      </div> 
-    ),
+    description: 'Find out how to use PeriFlow in the form of a Docker image.'
   },
 ];
 
-// function WebDevCard({ name, Svg, url, description }) {
-//   return (
-//     <div className="col col--6 margin-bottom--lg">
-//       <div className={clsx('card')}>
-//         <div className={clsx('card__image')}>
-//           <Link to={url.page}>
-//             <Svg className={styles.featureSvg} role="img"/>
-//           </Link>
-//         </div>
-//         <div className="card__body">
-//           <h3>{name}</h3>
-//           <p>{description}</p>
-//         </div>
-//         <div className="card__footer">
-//           <div className="button-group button-group--block">
-//             <Link className="button button--secondary" to={url.page}>
-//               <Translate id="special.tryItButton">View Now!</Translate>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+interface WebDevCardProps {
+  name: string;
+  png: string;
+  url: {
+    page: string;
+  };
+  description: string;
+}
 
-function WebDevCard({ name, png, url, description }) {
+const WebDevCard: React.FC<WebDevCardProps> = props => {
   return (
     <div className="col col--6 margin-bottom--lg">
-      <div className={clsx('card')}>
-        <div className={clsx('card__image')}>
-          <Link to={url.page}>
-            <img src={png} className={styles.featurePng}/>
-          </Link>
-        </div>
-        <div className="card__body">
-          <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="card__footer">
-          <div className="button-group button-group--block">
-            <Link className="button button--secondary" to={url.page}>
-              <Translate id="special.tryItButton">View Now!</Translate>
-            </Link>
+      <div className={styles.cardContainer}>
+        <Link to={props.url.page} style={{textDecoration: "none"}}>
+          <div className={styles.cardImage}>
+            <img src={props.png} className={styles.featurePng}/>
           </div>
-        </div>
+          <div className={styles.cardBody}>
+            <h3>{props.name}</h3>
+            <p>{props.description}</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
