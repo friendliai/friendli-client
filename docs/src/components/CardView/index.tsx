e34c2f95ac21299f52dddee15c2d40c87433e286
kooyunmo <yunmorning@friendli.ai>
2023-08-28 16:05:26 +0900

    support dark mode view

--- docs/src/components/CardView/index.tsx
+++ docs/src/components/CardView/index.tsx
@@ -1,8 +1,7 @@
-/* eslint-disable global-require */
-
 import React from 'react';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
+import {useColorMode} from '@docusaurus/theme-common';
 
 import cloud_img from '@site/static/img/docs/tutorials/cloud.png';
 import container_img from '@site/static/img/docs/tutorials/container.png';
@@ -37,14 +36,24 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = props => {
+  const {colorMode, setColorMode} = useColorMode();
+
   return (
     <div className="col col--6 margin-bottom--lg">
-      <div className={styles.cardContainer}>
+      <div
+        className={`${styles.cardContainer} ${
+          colorMode === 'light' ? styles.cardContainerLight : styles.cardContainerDark
+        }`}
+      >
         <Link to={props.url.page} style={{textDecoration: "none"}}>
           <div className={styles.cardImage}>
             <img src={props.png} className={styles.featurePng}/>
           </div>
-          <div className={styles.cardBody}>
+          <div
+            className={`${styles.cardBody} ${
+              colorMode === 'light' ? styles.cardBodyLight : styles.cardBodyDark
+            }`}
+          >
             <h3>{props.name}</h3>
             <p>{props.description}</p>
           </div>
